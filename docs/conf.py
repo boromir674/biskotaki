daa4ccf3e1c166b20b0bc73799f4c1ff5e3f3301
konstantinos <boromir674@hotmail.com>
2023-02-12 19:26:31 +0200

    release(semantic_version): bump version to 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ def get_templated_vars():
             package_name='biskotaki',
             author_name='Konstantinos Lampridis',
             year='2023',
-            version='0.0.1',
+            version='0.1.1',
             github_username='boromir674',
             repo_name='biskotaki',
         ),
